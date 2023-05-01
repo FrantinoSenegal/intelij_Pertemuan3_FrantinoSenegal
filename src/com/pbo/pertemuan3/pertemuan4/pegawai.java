daa2ea05cd88f25c54ee1375845be07ca4497e84
FrantinoSenegal <131945387+FrantinoSenegal@users.noreply.github.com>
2023-05-01 19:17:19 +0700

    Add files via upload

---
+++ src/com/pbo/pertemuan3/pertemuan4/pegawai.java
@@ -0,0 +1,12 @@
+package com.pbo.pertemuan3.pertemuan4;
+
+public class pegawai {
+    String nama;
+    int nip;
+
+    public void showinfo(){
+        System.out.println("Nama Pegawai : "+nama);
+        System.out.println("NIP : "+nip);
+    }
+    public void extrainfo(){}
+}
